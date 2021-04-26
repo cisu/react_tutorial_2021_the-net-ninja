7f7422dc7882f9802c17da6336daa5c19b41b4fd
cisu <49948070+cisu@users.noreply.github.com>
2021-04-27 00:27:41 +0300

    Multiple Components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 
 import './App.css';
 
+import Navbar from './Navbar';
+import Home from './Home';
+
+
+
+
 function App() {
 
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  
-  // we can't do this 
-  // const person = {name: 'george', age: 30}
-
-  const link = 'https://www.google.com'
 
   return (
     <div className="App">
+      <Navbar />
+     
      <div className="content">
-       <h1>{title}</h1>
-       <p>Liked {likes} times</p>
-
-       {/* <p>{person}</p> */}
-
-      <p>{ 10 }</p>
-      <p>{ 'Hello ninjas' }</p>
-      <p>{ [1,2,3,4,5] }</p>
-      <p>{ Math.random() * 10 }</p>
-
-      <a href={link}>google</a>
-
-     </div>
+     <Home />
+    </div>
     </div>
   );
 }
