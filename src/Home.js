ef13389356163fa6043366d604653c8015df5fa7
cisu <49948070+cisu@users.noreply.github.com>
2021-04-27 10:42:17 +0300

    Click Events

--- src/Home.js
+++ src/Home.js
@@ -14,9 +14,30 @@ const Home = () => {
         <div className='home'>
             <h2>Homepage</h2>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => {handleClickAgain('mario', e)}}>Click me again</button>
+            <button onClick={(e) => {handleClickAgain('mario', e )}}>Click me again</button>
         </div>
-    )
+    )    
 }
 
 export default Home
+
+
+/*
+Event objet inside these functions when an event occurs
+
+In this because we don't automatically get the event object as a first argument
+
+This below arrow function take the event object automatically, then if we want to use it inside our custom function we just pass it in as an argument like this 
+
+(e) => {handleClickAgain('mario', e )}
+
+And then we use it inside the function above the return like this
+(see the e)
+   const handleClickAgain = (name , e) => {
+        console.log(`Hello ${name}`, e.target);
+    }
+
+<button onClick={(e) => {handleClickAgain('mario', e )}}>Click me again</button>
+
+
+*/
