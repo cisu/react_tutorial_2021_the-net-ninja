1d8a8f570a92a349e7e0040b0006571953b7b277
cisu <49948070+cisu@users.noreply.github.com>
2021-04-27 21:15:53 +0300

    Making a POST Request

--- src/Create.js
+++ src/Create.js
@@ -6,12 +6,24 @@ const Create = () => {
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('mario')
 
+    const [isPending, setIsPending] = useState(false)
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
 
-        console.log(blog);
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type" : "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false)
+        })
+
     
     }
 
@@ -47,10 +59,10 @@ const Create = () => {
                     <option value="mario">Mario</option>
                     <option value="yoshi">Yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                { !isPending && <button>Add Blog</button>}
+                { isPending && <button disabled>Adding Blog...</button>}
+               
+                
             </form>
         </div>
     )
